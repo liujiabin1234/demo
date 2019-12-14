8223763e3036e98de805f66821512d883caa681f
liujiabin <liujiabin@123>
2019-12-14 18:31:35 +0800

    websocket搭建

--- src/main/java/com/example/demo/websocket/CheckCenterController.java
+++ src/main/java/com/example/demo/websocket/CheckCenterController.java
@@ -1,5 +1,8 @@
 package com.example.demo.websocket;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.session.Session;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
@@ -14,16 +17,31 @@ import java.util.Map;
 @Controller
 @RequestMapping("/checkcenter")
 public class CheckCenterController {
+    @Autowired
+    WebSocketServer webSocketServer;
 
-    @RequestMapping(value = "/pushVideoListToWeb", method = RequestMethod.POST, consumes = "application/json")
+    @RequestMapping(value="/pushVideoListToWeb",method=RequestMethod.POST,consumes = "application/json")
     @ResponseBody
-    public Map<String, Object> pushVideoListToWeb(@RequestBody Map<String, Object> param) {
-        Map<String, Object> result = new HashMap<String, Object>();
-
+    public Map<String,Object> pushVideoListToWeb(@RequestBody Map<String,Object> param) {
+        Map<String,Object> result =new HashMap<String,Object>();
         try {
-            WebSocketServer.sendInfo("有新客户呼入,sltAccountId:" + "222222222222");
+//            WebSocketServer.sendInfo("有新客户呼入,sltAccountId:"+CommonUtils.getValue(param, "sltAccountId"));
             result.put("operationResult", true);
-        } catch (IOException e) {
+        }catch (Exception e) {
+            result.put("operationResult", true);
+        }
+        return result;
+    }
+
+    @RequestMapping(value="/starManager")
+    @ResponseBody
+    public Map<String,Object> starManager() {
+        Map<String,Object> result =new HashMap<String,Object>();
+        try {
+            Session session = SecurityUtils.getSubject().getSession();
+            webSocketServer.onOpen((javax.websocket.Session) session);
+            result.put("operationResult", true);
+        }catch (Exception e) {
             result.put("operationResult", true);
         }
         return result;
--- src/main/java/com/example/demo/websocket/WebSocketServer.java
+++ src/main/java/com/example/demo/websocket/WebSocketServer.java
@@ -23,7 +23,6 @@ public class WebSocketServer {
     private static int onlineCount = 0;
     //concurrent包的线程安全Set，用来存放每个客户端对应的MyWebSocket对象。
     private static CopyOnWriteArraySet<WebSocketServer> webSocketSet = new CopyOnWriteArraySet<WebSocketServer>();
-
     //与某个客户端的连接会话，需要通过它来给客户端发送数据
     private Session session;
 
@@ -41,22 +40,6 @@ public class WebSocketServer {
         } catch (IOException e) {
             log.error("websocket IO异常");
         }
-    }
-    //	//连接打开时执行
-    //	@OnOpen
-    //	public void onOpen(@PathParam("user") String user, Session session) {
-    //		currentUser = user;
-    //		System.out.println("Connected ... " + session.getId());
-    //	}
-
-    /**
-     * 连接关闭调用的方法
-     */
-    @OnClose
-    public void onClose() {
-        webSocketSet.remove(this);  //从set中删除
-        subOnlineCount();           //在线数减1
-        log.info("有一连接关闭！当前在线人数为" + getOnlineCount());
     }
 
     /**
@@ -78,21 +61,6 @@ public class WebSocketServer {
         }
     }
 
-    /**
-     * @param session
-     * @param error
-     */
-    @OnError
-    public void onError(Session session, Throwable error) {
-        log.error("发生错误");
-        error.printStackTrace();
-    }
-
-
-    public void sendMessage(String message) throws IOException {
-        this.session.getBasicRemote().sendText(message);
-    }
-
 
     /**
      * 群发自定义消息
@@ -107,6 +75,39 @@ public class WebSocketServer {
             }
         }
     }
+
+    public void sendMessage(String message) throws IOException {
+        this.session.getBasicRemote().sendText(message);
+    }
+
+
+    //连接打开时执行
+//    @OnOpen
+//    public void onOpen(@PathParam("user") String user, Session session) {
+//        currentUser = user;
+//        System.out.println("Connected ... " + session.getId());
+//    }
+
+    /**
+     * 连接关闭调用的方法
+     */
+    @OnClose
+    public void onClose() {
+        webSocketSet.remove(this);  //从set中删除
+        subOnlineCount();           //在线数减1
+        log.info("有一连接关闭！当前在线人数为" + getOnlineCount());
+    }
+
+    /**
+     * @param session
+     * @param error
+     */
+    @OnError
+    public void onError(Session session, Throwable error) {
+        log.error("发生错误");
+        error.printStackTrace();
+    }
+
 
     public static synchronized int getOnlineCount() {
         return onlineCount;
