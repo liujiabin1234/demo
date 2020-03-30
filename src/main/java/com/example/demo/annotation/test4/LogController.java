f93e51ce120f5631e801b702be752330e57c5774
liujiabin1234 <1206784943@qq.com>
2020-03-30 23:48:20 +0800

    练习git合并

--- src/main/java/com/example/demo/annotation/test4/LogController.java
+++ src/main/java/com/example/demo/annotation/test4/LogController.java
@@ -23,6 +23,11 @@ public class LogController {
         return "success";
     }
 
+    @RequestMapping("/test3")
+    public String test3() {
+        logService.testLogAspect();
+        return "success";
+    }
 }
 
 
