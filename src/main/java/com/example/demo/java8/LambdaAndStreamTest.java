578bbc9df68fce863a588739b0e4ee3056d521c8
liujiabin <1206784943@qq.com>
2020-10-21 13:25:02 +0800

    java8 stream

--- src/main/java/com/example/demo/java8/LambdaAndStreamTest.java
+++ src/main/java/com/example/demo/java8/LambdaAndStreamTest.java
@@ -1,13 +1,102 @@
 package com.example.demo.java8;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.example.demo.shiro.User;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.junit.Test;
 
+import java.math.BigDecimal;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class LambdaAndStreamTest {
+
+    @Test
+    public void test13() {
+        User tom2 = new User("Tom", 1);
+        User tom1 = new User("Tom", 2);
+        User tom = new User("Tom", 3);
+        User dick2 = new User("Dick", 3);
+        User dick1 = new User("Dick", 3);
+        User dick = new User("Dick", 3);
+        List<User> users = new ArrayList<>();
+        users.add(tom);
+        users.add(tom2);
+        users.add(tom1);
+        users.add(dick);
+        users.add(dick2);
+        users.add(dick1);
+        final String username = users.stream().map(User::getUsername).collect(Collectors.joining(","));
+        System.out.println(username);
+    }
+
+    @Test
+    public void test12() {
+        User tom2 = new User("Tom", 1);
+        User tom1 = new User("Tom", 2);
+        User tom = new User("Tom", 3);
+        User dick2 = new User("Dick", 3);
+        User dick1 = new User("Dick", 3);
+        User dick = new User("Dick", 3);
+        List<User> users = new ArrayList<>();
+        users.add(tom);
+        users.add(tom2);
+        users.add(tom1);
+        users.add(dick);
+        users.add(dick2);
+        users.add(dick1);
+        Map<String, Integer> dotMap = users.stream().collect(Collectors.groupingBy(User::getUsername, Collectors.summingInt(User::getAge)));
+        System.out.println(JSONObject.toJSONString(dotMap));
+    }
+
+    @Test
+    public void test11() {
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "");
+        String num = map.getOrDefault("num", "5000");
+        System.out.println(num);
+    }
+
+    @Test
+    public void test10() {
+        final String resStr = "{\"username\":\"Tom\",\"password\":\"123456\"}";
+        final JSONObject jsonObject = JSONObject.parseObject(resStr);
+        final User user = JSON.toJavaObject(jsonObject, User.class);
+        System.out.println(JSONObject.toJSONString(user));
+    }
+
+    @Test
+    public void test9() {
+        BigDecimal bigDecimal = NumberUtils.createBigDecimal("1");
+        BigDecimal bigDecimal1 = new BigDecimal("1");
+        System.out.println(bigDecimal);
+        System.out.println(bigDecimal1);
+    }
+
+    @Test
+    public void test8() {
+        ArrayList<User> users = new ArrayList<>();
+        users.add(new User("Tom", 10));
+        users.add(new User("Tom2", 20));
+        users.add(new User("Tom3", 30));
+        users.add(new User("Tom4", 40));
+        users.removeIf(user -> (user.getAge() > 18));
+        System.out.println(JSONArray.toJSONString(users));
+    }
+
+    @Test
+    public void test7() {
+        String onlineShopId = "";
+//        Assert.isNull(onlineShopId, "online shop id could not be null");
+        User user = new User();
+        String s = Optional.ofNullable(user).map(User::getUsername).orElse(null);
+        Integer integer = Optional.ofNullable(user).map(User::getAge).orElse(2);
+//        Integer integer2 = Optional.ofNullable(user).map(User::getAge).orElseThrow(() -> new IllegalArgumentException("参数异常"));
+        System.out.println(s + "/" + integer);
+    }
 
     //集合排序
     @Test
