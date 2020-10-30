07be3c2fa2afdafe93ba1e16ec3010ac2ccbba9a
liujiabin <1206784943@qq.com>
2020-10-30 13:31:12 +0800

    RedissonClient

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import com.example.demo.mybatis.dto.LargeCount;
 import com.example.demo.mybatis.service.LargeCountService;
 import com.example.demo.redis.RedisCacheClient;
 import org.junit.Test;
@@ -30,13 +29,13 @@ public class DemoApplicationTests {
 
     @Test
     public void test4() throws InterruptedException {
-        RLock rLock = redissonClient.getLock(String.format("lj:lock:o2ot:ow-order:pay:%s","Tom"));
+        RLock rLock = redissonClient.getLock(String.format("lj:lock:o2ot:ow-order:pay:%s", "Tom"));
 //        rLock.lock();
 //        rLock.lock(1, TimeUnit.MINUTES);
         // 尝试加锁，最多等待1秒，上锁以后1分钟自动解锁，tryLock方法有返回值
         boolean b = rLock.tryLock(1, 1, TimeUnit.MINUTES);
         System.out.println(b);
-        unlock(String.format("lj:lock:o2ot:ow-order:pay:%s","Tom"));
+        unlock(String.format("lj:lock:o2ot:ow-order:pay:%s", "Tom"));
     }
 
     public void unlock(String lockKey) {
@@ -56,38 +55,38 @@ public class DemoApplicationTests {
     @Test
     public void test3() {
 
-//        redisCacheClient.boundZSetOps("三国2").add("曹操", 1);
-//        redisCacheClient.boundZSetOps("三国2").add("曹操2", 2);
-//        redisCacheClient.boundZSetOps("三国2").add("曹操3", 3);
-//        redisCacheClient.boundZSetOps("三国2").add("曹操4", 4);
-//        redisCacheClient.boundZSetOps("三国2").add("曹操5", 5);
-//        //按照分数正序排序
-//        redisCacheClient.opsForZSet().rangeByScoreWithScores("三国2", 1, 50, 0, 3);
-//        //按照分数倒序排序
-//        redisCacheClient.opsForZSet().reverseRangeByScoreWithScores("三国2", 1, 50, 0, 3);
-//
-//        redisCacheClient.boundSetOps("三国").add("刘备");
-//        redisCacheClient.boundSetOps("三国").add("孙权");
-//        redisCacheClient.boundSetOps("三国").members();
-//        redisCacheClient.boundSetOps("三国").remove("孙权");
-//        redisCacheClient.delete("三国");
-//
-//        redisCacheClient.boundListOps("桃园三结义").rightPush("刘备");
-//        redisCacheClient.boundListOps("桃园三结义").rightPush("关羽");
-//        redisCacheClient.boundListOps("桃园三结义").rightPush("张飞");
-//        redisCacheClient.boundListOps("桃园三结义").range(0, 10);
-//        redisCacheClient.boundListOps("桃园三结义").index(1);
-//        redisCacheClient.boundListOps("桃园三结义").remove(1, "关羽");
-//
-//        redisCacheClient.boundHashOps("西游记").put("老大", "唐僧");
-//        redisCacheClient.boundHashOps("西游记").put("老二", "悟空");
-//        redisCacheClient.boundHashOps("西游记").put("老三", "八戒");
-//        redisCacheClient.boundHashOps("西游记").put("老四", "沙僧");
-//        redisCacheClient.boundHashOps("西游记").keys();
-//        redisCacheClient.boundHashOps("西游记").values();
-//        redisCacheClient.boundHashOps("西游记").get("老四");
-//        redisCacheClient.boundHashOps("西游记").delete("c", "a");
-//        redisCacheClient.boundHashOps("西游记").delete();
+        redisCacheClient.boundZSetOps("三国2").add("曹操", 1);
+        redisCacheClient.boundZSetOps("三国2").add("曹操2", 2);
+        redisCacheClient.boundZSetOps("三国2").add("曹操3", 3);
+        redisCacheClient.boundZSetOps("三国2").add("曹操4", 4);
+        redisCacheClient.boundZSetOps("三国2").add("曹操5", 5);
+        //按照分数正序排序
+        redisCacheClient.opsForZSet().rangeByScoreWithScores("三国2", 1, 50, 0, 3);
+        //按照分数倒序排序
+        redisCacheClient.opsForZSet().reverseRangeByScoreWithScores("三国2", 1, 50, 0, 3);
+
+        redisCacheClient.boundSetOps("三国").add("刘备");
+        redisCacheClient.boundSetOps("三国").add("孙权");
+        redisCacheClient.boundSetOps("三国").members();
+        redisCacheClient.boundSetOps("三国").remove("孙权");
+        redisCacheClient.delete("三国");
+
+        redisCacheClient.boundListOps("桃园三结义").rightPush("刘备");
+        redisCacheClient.boundListOps("桃园三结义").rightPush("关羽");
+        redisCacheClient.boundListOps("桃园三结义").rightPush("张飞");
+        redisCacheClient.boundListOps("桃园三结义").range(0, 10);
+        redisCacheClient.boundListOps("桃园三结义").index(1);
+        redisCacheClient.boundListOps("桃园三结义").remove(1, "关羽");
+
+        redisCacheClient.boundHashOps("西游记").put("老大", "唐僧");
+        redisCacheClient.boundHashOps("西游记").put("老二", "悟空");
+        redisCacheClient.boundHashOps("西游记").put("老三", "八戒");
+        redisCacheClient.boundHashOps("西游记").put("老四", "沙僧");
+        redisCacheClient.boundHashOps("西游记").keys();
+        redisCacheClient.boundHashOps("西游记").values();
+        redisCacheClient.boundHashOps("西游记").get("老四");
+        redisCacheClient.boundHashOps("西游记").delete("c", "a");
+        redisCacheClient.boundHashOps("西游记").delete();
 
     }
 
